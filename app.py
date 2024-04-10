03cfc92d39bccafa80a93c6eae7702815175e145
edgardesnos <edgar.desnos@mail.mcgill.ca>
2024-04-10 01:35:44 -0400

    version 2 commit

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from chat import LLMChatBot
 import streamlit as st
 
+DEFAULT_BOT_MESSAGE = {
+    "role": "assistant",
+    "content": "**Hello!**\n\nI am a conversational agent in charge of helping you with any question you may have about the MadKudu platform.\n\nTo start conversing with me, add your OpenAI API Key to the field above, then feel free to ask me about anything, and I'll do my best to assist you.",
+}
+
 # Init session state
 if "api_key" not in st.session_state:
     st.session_state.api_key = None
 if "model" not in st.session_state:
     st.session_state.model = None
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+    st.session_state.messages = [DEFAULT_BOT_MESSAGE]
 
 st.title("MadKudu Support Assistant")
 
@@ -16,7 +21,7 @@ api_key = st.text_input("OpenAI API Key", type="password")
 # Reset chats and model when API key is changed
 if api_key != st.session_state.api_key and api_key:
     st.session_state.api_key = api_key
-    st.session_state.messages = []
+    st.session_state.messages = [DEFAULT_BOT_MESSAGE]
     st.session_state.model = LLMChatBot(api_key)
 
 # Display history of chat messsages
@@ -30,6 +35,15 @@ if prompt := st.chat_input("What is your question?", disabled=not api_key):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
     response = st.session_state.model.ask_question(prompt)
+    doc_list = ", ".join(
+        [
+            f"[{document.metadata['title'].removesuffix(' – MadKudu')}]({document.metadata['source']})"
+            for document in response["context"]
+        ]
+    )
+    response_with_sources = response["answer"] + f"\n\n**Relevant links**: {doc_list}"
     with st.chat_message("assistant"):
-        st.markdown(response)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        st.markdown(response_with_sources)
+    st.session_state.messages.append(
+        {"role": "assistant", "content": response_with_sources}
+    )
--- chat.py
+++ chat.py
@@ -1,35 +1,73 @@
-__import__("pysqlite3")
-import sys
-sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
-
-from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
-from langchain import hub
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
+from langchain.chains import create_history_aware_retriever
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.messages import HumanMessage
+from langchain_community.vectorstores import FAISS
 
-DB_DIRECTORY = "./chroma_db"
+DB_DIRECTORY = "./document_db"
+
+CONTEXTUALIZE_Q_SYSTEM_PROMPT = """Given a chat history and the latest user question \
+which might reference context in the chat history, formulate a standalone question \
+which can be understood without the chat history. Do NOT answer the question, \
+just reformulate it if needed and otherwise return it as is."""
+CONTEXTUALIZE_Q_PROMPT = ChatPromptTemplate.from_messages(
+    [
+        ("system", CONTEXTUALIZE_Q_SYSTEM_PROMPT),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+
+QA_SYSTEM_PROMPT = """You are an assistant for question-answering tasks about the MadKudu platform. \
+You speak using a professional and respectful tone. \
+Use the following pieces of retrieved context to answer the question. \
+If you don't know the answer, just say that you don't know. \
+Use three sentences maximum and keep the answer concise but clear.\
+
+{context}"""
+QA_PROMPT = ChatPromptTemplate.from_messages(
+    [
+        ("system", QA_SYSTEM_PROMPT),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+
 
 class LLMChatBot:
 
     def __init__(self, api_key) -> None:
-        llm = ChatOpenAI(model="gpt-3.5-turbo-0125", api_key=api_key)
-        
-        vectorstore = Chroma(persist_directory=DB_DIRECTORY, embedding_function=OpenAIEmbeddings(api_key=api_key))
-        retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
+        llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0, api_key=api_key)
 
-        prompt = hub.pull("rlm/rag-prompt")
+        vectorstore = FAISS.load_local(
+            folder_path=DB_DIRECTORY,
+            embeddings=OpenAIEmbeddings(api_key=api_key),
+            allow_dangerous_deserialization=True,
+        )
+        retriever = vectorstore.as_retriever(
+            search_type="similarity_score_threshold",
+            search_kwargs={"score_threshold": 0.3, "k": 6},
+        )
 
-        def format_docs(docs):
-            return "\n\n".join(doc.page_content for doc in docs)
+        history_aware_retriever = create_history_aware_retriever(
+            llm, retriever, CONTEXTUALIZE_Q_PROMPT
+        )
 
-        self.rag_chain = (
-        {"context": retriever | format_docs, "question": RunnablePassthrough()}
-        | prompt
-        | llm
-        | StrOutputParser()
-    )
+        question_answer_chain = create_stuff_documents_chain(llm, QA_PROMPT)
+
+        self.rag_chain = create_retrieval_chain(
+            history_aware_retriever, question_answer_chain
+        )
+        self.chat_history = []
 
     def ask_question(self, question):
-        return self.rag_chain.invoke(question)
+        chain_response = self.rag_chain.invoke(
+            {"input": question, "chat_history": self.chat_history}
+        )
+        self.chat_history.extend(
+            [HumanMessage(content=question), chain_response["answer"]]
+        )
+        return chain_response
